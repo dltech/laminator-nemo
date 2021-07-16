0408761e09231cf94695bb1ec0fe798f97c518f3
Mihail <dltech174@gmail.com>
2021-07-16 23:06:09 +0700

    *

--- lib/ds18b20.h
+++ lib/ds18b20.h
@@ -20,15 +20,13 @@
 
 #include "inttypes.h"
 
+/* Configuration macro */
 // Rom commands
 #define SEARCH_ROM          0xf0
 #define READ_ROM            0x33
 #define MATCH_ROM           0x55
 #define SKIP_ROM            0xcc
 #define ALARM_SEARCH        0xec
-
-#define FAMILY_CODE         0x28
-
 // Function commands
 #define CONVERT_T           0x44
 #define READ_SCRATCHPAD     0xbe
@@ -36,30 +34,32 @@
 #define COPY_SCRATCHPAD     0x48
 #define RECALL_E2           0xb8
 #define READ_POWER_SUPPLY   0xb4
-
+// ds18b20 family code
+#define FAMILY_CODE         0x28
 // configuration
 #define RESOL_9BIT          0x1f
 #define RESOL_10BIT         0x3f
 #define RESOL_11BIT         0x5f
 #define RESOL_12BIT         0x7f
-
-// mcu related
-#define DS_PORT GPIOA
-#define DS_PIN  2
 // timings
+// for bit read/write
 #define WRITE0T     70
 #define WRITE1T1    10
 #define WRITE1T2    70
 #define READT       10
 #define TIMESLOT    100
-
+// for init pulse
 #define RESET_PULSE     500
 #define RESET_WAIT      50
 #define INIT_PULSE_MIN  50
 #define INIT_PULSE_MAX  300
-
+// ds port latency
 #define PULSE_DELAY  4
 
+/* settings */
+// mcu related
+#define DS_PORT GPIOA
+#define DS_PIN  2
 // set the resolution
 #define DEFAULT_RESOL       RESOL_9BIT
 
